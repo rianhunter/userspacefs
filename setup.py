cf7e5dda19d1d246ebe5bd1754a0a888412e1fe5
Rian Hunter <rian@alum.mit.edu>
2018-10-25 09:13:49 -0700

    Release 1.0.13

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="userspacefs",
-    version='1.0.12',
+    version='1.0.13',
     author="Rian Hunter",
     author_email="rian@alum.mit.edu",
     description="Cross-platform user-space file systems for Python",
